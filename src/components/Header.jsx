875a0af5f28eb20b5a3a4b75facad3114e75ab4d
Leandro <lean3dro@gmail.com>
2024-10-15 20:23:48 -0300

    acualizando detalles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
     <div className={Styles.textContainer}>
       <h2 className={Styles.eventTitle}>¡Estás Invitado!</h2>
       <p className={Styles.invitationText}>
-        ME encantaría que seas parte de este momento tan especial para mí, ¡falta poco!
+        Me encantaría que seas parte de este momento tan especial para mí, ¡falta poco!
       </p>
     </div>
   )
