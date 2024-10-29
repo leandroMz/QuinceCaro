e07ed97baca19d9255507ee3225eb77645cf50f0
Leandro <lean3dro@gmail.com>
2024-10-28 22:30:20 -0300

    ima

--- src/components/SecondaryImages.jsx
+++ src/components/SecondaryImages.jsx
@@ -1,3 +1,6 @@
+import img1 from '/img1.jpg';
+import img2 from '/img2.jpg';
+import img3 from '/img3.jpg';
 import Styles from './SecondaryImages.module.css';
 
 export const SecondaryImages = () => {
@@ -5,10 +8,10 @@ export const SecondaryImages = () => {
     <div className={Styles.imageSecondaryContainer}>
         <>
         <div className={Styles.headSecondary}>
-          <img src="/img1.jpg" alt="Quinceañera 1" className={Styles.image} />
-          <img src="/img2.jpg" alt="Quinceañera 2" className={Styles.image} />
+          <img src={img1}alt="Quinceañera 1" className={Styles.image} />
+          <img src={img2} alt="Quinceañera 2" className={Styles.image} />
           </div>
-          <img src="/img3.jpg" alt="Quinceañera 3" className={Styles.image} />
+          <img src={img3} alt="Quinceañera 3" className={Styles.image} />
         </>
     </div>
   );
