b8a62523f22c4087d06d61ccbf0f666e4d75c943
Leandro <lean3dro@gmail.com>
2024-10-28 22:34:37 -0300

    imagenes

--- src/components/SecondaryImages.jsx
+++ src/components/SecondaryImages.jsx
@@ -6,13 +6,13 @@ import Styles from './SecondaryImages.module.css';
 export const SecondaryImages = () => {
   return (
     <div className={Styles.imageSecondaryContainer}>
-        <>
+      <>
         <div className={Styles.headSecondary}>
-          <img src={img1}alt="Quinceañera 1" className={Styles.image} />
+          <img src={img1} alt="Quinceañera 1" className={Styles.image} />
           <img src={img2} alt="Quinceañera 2" className={Styles.image} />
-          </div>
-          <img src={img3} alt="Quinceañera 3" className={Styles.image} />
-        </>
+        </div>
+        <img src={img3} alt="Quinceañera 3" className={Styles.image} />
+      </>
     </div>
   );
 };
