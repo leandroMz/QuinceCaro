8dcfca8554065c9854a946db8fa4702f50b7f2ed
Leandro <lean3dro@gmail.com>
2024-10-30 14:02:43 -0300

    suga

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useRef } from 'react';
 import { Home } from './routes/Home';
 import { Envelope } from './components/Envelope';
 import './App.css';
+import MainImage from './components/MainImage';
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,11 +17,11 @@ function App() {
 
   return (
     <>
-      {!isOpen ? (
+      <div style={{ display: isOpen ? 'none' : 'block' }}>
         <Envelope onOpen={handleOpen} />
-      ) : (
-        <Home audioRef={audioRef} />
-      )}
+      </div>
+      <MainImage style={{ display: isOpen ? 'block' : 'none' }} /> {/* Solo se muestra en Home */}
+      {isOpen && <Home audioRef={audioRef} />}
       <audio ref={audioRef} src="/musica.mp3" loop />
     </>
   );
